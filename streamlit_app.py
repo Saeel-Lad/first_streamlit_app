14c2f197bbe5f7ad1821e9083df50b230784dad6
Saeel-Lad <130133340+Saeel-Lad@users.noreply.github.com>
2023-04-10 14:23:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,10 +61,6 @@ if streamlit.button('Get Fruit Load List'):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
-streamlit.stop()
-
-
-
 
 def insert_row_snowflake(new_fruit):
   my cnx.cursor() as my_cursor:
